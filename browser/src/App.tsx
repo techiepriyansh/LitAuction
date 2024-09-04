01c639d0d42a09084409a830188e77abf09eee25
Priyansh Rathi <techiepriyansh@gmail.com>
2024-09-05 00:51:34 +0530

    Better routing file structure

--- browser/src/App.tsx
+++ browser/src/App.tsx
@@ -1,14 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-import Home from './pages/Home';
 import Auctioneer from './pages/Auctioneer';
 import Bidder from './pages/Bidder';
 
-
-import { litMain } from "./lit/litClient";
-import { signMain } from "./sign/signClient"
-
 function App() {
+  const Home = () => {
+    return (
+        <div className="h-full w-full flex flex-col items-center justify-center">
+          <div className="h-1/2 w-auto flex flex-col">
+            <div className="h-48 w-full flex flex-col justify-center items-center text-8xl">Lit Auction</div>
+            <div className="h-full flex flex-col items-start text-3xl space-y-1 py-2 pl-5 text-[#00a2e7]">
+              <div> <Link to="/auctioneer"> ⤷ Auctioneer </Link> </div>
+              <div> <Link to="/bidder"> ⤷ Bidder </Link> </div>
+            </div>
+          </div>
+        </div>
+    );
+  }
+
   return (
     <Router>
       <Routes>
