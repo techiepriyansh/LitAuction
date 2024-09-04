97fb16dbe9e0f4a2c287094c2f2dc691f1c6325e
Priyansh Rathi <techiepriyansh@gmail.com>
2024-09-05 00:58:59 +0530

    Create template Host Action page

--- browser/src/pages/auctioneer/HostAuction.tsx
+++ browser/src/pages/auctioneer/HostAuction.tsx
@@ -1,5 +1,13 @@
 function HostAuction() {
-    return <>Host Auction</>
+    return (
+      <div className="h-full w-full flex flex-col">
+        <div className="h-24 w-full flex flex-col justify-center text-5xl pl-5">Host Auction</div>
+        <div className="h-full w-full flex flex-col items-start space-y-1 py-2 pl-10">
+          <div>  Things go here </div>
+          <div> and here and ...</div>
+        </div>
+      </div>
+    );
 }
 
 export default HostAuction;
