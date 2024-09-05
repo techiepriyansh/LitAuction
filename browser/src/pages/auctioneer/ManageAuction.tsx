4f9b26e771b4aa2345e05bebd35a5abb0b92b7e1
Priyansh Rathi <techiepriyansh@gmail.com>
2024-09-05 18:47:13 +0530

    Start auction works

--- browser/src/pages/auctioneer/ManageAuction.tsx
+++ browser/src/pages/auctioneer/ManageAuction.tsx
@@ -59,16 +59,39 @@ function ManageAuction() {
         consoleLog("Randomness generated. Keep it somewhere safe. You will need it for all subsequent actions including bid claim.")
     }
 
-    const getAuxWalletAddress = async () => {
+    const computeRandBytesHex = (): string => {
         const userRandHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(auctionData.userRandomness));
         const auctionIdHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(auctionData.auctionId));
         const randBytes = ethers.utils.keccak256(ethers.utils.concat([userRandHash, auctionIdHash]));
         const randBytesHex = ethers.utils.hexlify(userRandHash); // TODO: replace with randBytes
+        return randBytesHex;
+    }
 
+    const getAuxWalletAddress = async () => {
+        const randBytesHex = computeRandBytesHex();
         consoleLog("Generating auxiliary wallet address...")
         const { retVal } = await litMain("genAuxWallet", { auctionId: auctionData.auctionId, userRand: randBytesHex });
         consoleLog(`Auxiliary wallet address: ${retVal.auxWalletAddress}`)
         consoleLog(`You will need to commit your NFT to this address before starting the auction.`)
+    }
+
+    const startAuction = async () => {
+        consoleLog("Starting auction...")
+        const { retStatus } = await litMain("hostStartAuction", { auctionId: auctionData.auctionId, userRand: computeRandBytesHex() });
+        switch (retStatus) {
+            case "success": {
+                consoleLog("Auction started successfully.");
+                break;
+            }
+            case "eAuctionAlreadyStarted": {
+                consoleLog("Auction already started.");
+                break;
+            }
+            case "eDidNotCommitNft": {
+                consoleLog("NFT not committed to auxiliary wallet. Please commit the NFT before starting the auction.");
+                break;
+            }
+        }
     }
 
     return (
@@ -133,6 +156,7 @@ function ManageAuction() {
                     </div>
                     <div className="mt-4 pl-10">
                         <button
+                            onClick={startAuction}
                             className="w-96 bg-[#00a2e7] text-white rounded-md px-4 py-2 hover:bg-[#00a8f0] hover:shadow-lg transition-all duration-300"
                         >
                             Start Auction
